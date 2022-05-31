591fee2591ede79d804d0cc2ff0d90cf54ddf41b
yel09 <mmmdagohoy2021@plm.edu.ph>
2022-06-01 01:13:25 +0800

    update
    Left Join

--- indexfiles/Stocks/stocks.php
+++ indexfiles/Stocks/stocks.php
@@ -1,6 +1,13 @@
 <?php
 
+    include 'connection.php';
+
+   
+
+
     
+
+
 
    
 
@@ -46,6 +53,60 @@
     <main>
 
         <section class="glass">
+
+           
+
+        <div class="product_one">
+
+        <h3>ADDED PRODUCTS</h3>
+
+        <?php 
+
+            $sql = "
+            SELECT 
+
+            *
+
+            FROM 
+                `modify_stocks` as s
+            LEFT JOIN 
+                `modify_products` as p ON s.PRODUCT_ID = p.PRODUCT_ID
+
+            ";
+
+                $result = $conn->query($sql);
+
+                
+
+                if ($result->num_rows > 0) {
+
+                // //output data of each row
+                    
+                while($row = $result->fetch_assoc()) {
+
+                        // print_r($row);
+
+                        // echo "id:" . $row["STUDENT_ID"] . "<br>"; 
+                        echo "PRODUCT NAME:&nbsp;" . $row["PRODUCT_NAME"] . "<br>";
+                        echo "PRODUCT DETAIL:&nbsp;" . $row["PRODUCT_DETAIL"] . "<br>";
+                        echo "QUANTITY:&nbsp;" . $row["PRODUCT_QYT"] . "<br>";
+                       
+                        
+                        
+
+                        }
+                        } else {
+                                echo "0 results";
+                            }
+           
+
+
+        ?>
+
+
+        </div>
+
+
 
             
 
