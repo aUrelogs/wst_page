b66007186563f4a40e66e4ea261bb6ba9f91f2c3
yel09 <mmmdagohoy2021@plm.edu.ph>
2022-06-03 05:55:26 +0800

    update

--- rootfolder/insidefolders/admin_stocks/stocks.php
+++ rootfolder/insidefolders/admin_stocks/stocks.php
@@ -62,7 +62,7 @@
     <header>
         
         <img src="images/logo.png" class="logo" style="width: 100px;">
-        <img class="logo" src="images/logoname.png" alt="logo" style="margin-right: 300px;">
+        <img class="logo" src="images/logoname.png" alt="logo" style="margin-right: 250px;">
         <nav>
             <ul class ="nav_links">
                 <li> <img src="images/userlogoo.png" class="logo" style="width: 20px;"> | <a href="http://localhost/wst_page/rootfolder/index.php">Admin Log Out</a></li>
@@ -111,7 +111,7 @@
                                         <tr>
                                         <td>
                                         <a href='edit_stock.php? id=$stocksIDs[$idx]'><button type= 'btn' class='button2'>EDIT</button></a> 
-                                        <a href='http://localhost/wst_page/rootfolder/insidefolders/admin_product/displayprod.php'><button type= 'btn' class='button2'>PRODUCTS</button></a>
+                                        <a href='http://localhost/wst_page/rootfolder/insidefolders/admin_product/displayprod.php'><button type= 'btn' class='button3'>PRODUCTS</button></a>
                                         &nbsp;
             
                                       
