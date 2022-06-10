e9e3c95e543246d19be7a8cea39521bac2b483ae
Vanbis <96293448+Vanbis@users.noreply.github.com>
2022-06-10 14:11:20 +0800

    Modified
    - navbar
    - color
    - background

--- rootfolder/insidefolders/customer_checkout/header.php
+++ rootfolder/insidefolders/customer_checkout/header.php
@@ -2,8 +2,8 @@
 
    <div class="flex">
 
-      <a href="#" class="logo">foodies</a>
-
+   <div><img src="images/logo.png" style="width: 100px;"><img src="images/logoname.png" alt="logo"></div>
+   
       <nav class="navbar">
          <a href="http://localhost/wst_page/rootfolder/">add products</a>
          <a href="http://localhost/wst_page/rootfolder/insidefolders/customer_pg_menu/products.php">view products</a>
