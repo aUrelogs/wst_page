a357b1bef79729b31d34e5d70c3f0ac303a51448
yel09 <mmmdagohoy2021@plm.edu.ph>
2022-06-11 16:37:30 +0800

    update order status

--- rootfolder/insidefolders/order_status/header.php
+++ rootfolder/insidefolders/order_status/header.php
@@ -2,12 +2,12 @@
 
    <div class="flex">
 
-      <a href="#" class="logo"><img src="images/logo.png" style="width: 90px;"></a>
-      <img class="logo" src="images/logoname.png" alt="logo" style="margin-right: 150px;">
+      <a href="#" class="logo"><img src="image/logo.png" style="width: 90px;"></a>
+      <img class="logo" src="image/logoname.png" alt="logo" style="margin-right: 150px;">
 
       <nav class="navbar">
          <a href="http://localhost/wst_page/rootfolder/">add products</a>
-         <a href="products.php">view products</a>
+         <a href="http://localhost/wst_page/rootfolder/insidefolders/customer_pg_menu/products.php">view products</a>
       </nav>
 
       <?php
