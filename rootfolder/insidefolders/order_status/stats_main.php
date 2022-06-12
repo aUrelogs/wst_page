412bd8ae555022eccd33d52418e5db5d31b3428e
yel09 <mmmdagohoy2021@plm.edu.ph>
2022-06-12 15:50:44 +0800

    update connection

--- rootfolder/insidefolders/order_status/stats_main.php
+++ rootfolder/insidefolders/order_status/stats_main.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>What's the Scoop | Customer Order Status</title>
+    <title>WHAT'S THE SCOOP? | Customer Order Status</title>
 
      <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -37,37 +37,37 @@
 
     <div id="ready">
   
-        <a href="#" > <img src="image/pack.gif" alt="pack" title="Packing"></a>
+        <a href="http://localhost/wst_page/rootfolder/insidefolders/order_status/to_pay.php" > <img src="image/pack.gif" alt="pack" title="Packing"></a>
 
     </div>
 
     <div class="to_pay">
         
-        <a href="#"><h3>To Pay</h3></a>
+        <a href="http://localhost/wst_page/rootfolder/insidefolders/order_status/to_pay.php"><h3>To Pay</h3></a>
 
     </div>
 
     <div id="set">
       
-        <a href="#"><img src="image/ship.gif" alt="ship" title="Ship"></a>
+        <a href="http://localhost/wst_page/rootfolder/insidefolders/order_status/to_ship.php"><img src="image/ship.gif" alt="ship" title="Ship"></a>
 
     </div>
 
     <div class="to_ship">
 
-        <a href="#"><h3>To Ship</h3></a>
+        <a href="http://localhost/wst_page/rootfolder/insidefolders/order_status/to_ship.php"><h3>To Ship</h3></a>
 
     </div>
 
     <div id="go">
 
-        <a href="#"><img src="image/receive.gif" alt="receive" title="Receive"></a>
+        <a href="http://localhost/wst_page/rootfolder/insidefolders/order_status/to_receive.php"><img src="image/receive.gif" alt="receive" title="Receive"></a>
   
     </div>
     
     <div class="to_receive">
 
-        <a href="#"><h3>To Receive</h3></a>
+        <a href="http://localhost/wst_page/rootfolder/insidefolders/order_status/to_receive.php"><h3>To Receive</h3></a>
     
     </div>
     
