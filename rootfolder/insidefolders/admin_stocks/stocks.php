7a8a30f9a31c087263eb709b4e9a3d572e00d808
yel09 <mmmdagohoy2021@plm.edu.ph>
2022-06-19 18:15:15 +0800

    update fix problem

--- rootfolder/insidefolders/admin_stocks/stocks.php
+++ rootfolder/insidefolders/admin_stocks/stocks.php
@@ -35,47 +35,7 @@
     }
 
 
-    // $sql = "
-    // SELECT
-    // *
-    // FROM 
-    //     `stocks` as s
-    // LEFT JOIN 
-    //     `cart` as p ON s.id = p.id
-
-    // ";
-
-    // $result = $conn->query($sql);
-
-    // if($result->num_rows > 0){
-
-    //     $total_stocks = 0;
-
-
-    //     $sidx = 0;
-
-    // while($row = $result->fetch_assoc()){
-
-    //     $id[$sidx] = $row["id"];
-    //     // $quantity[$sidx] = $row["quantity"];
-
-      
-
-    //     $total_stocks = ( $row['PRODUCT_QTY'] - $row['quantity']);
-
-    //     $sidx++;
-
-
-
-    // }
-
-
-
-
-
-    // }
-
-    
+ 
 
 
 
@@ -114,9 +74,8 @@
 
     </header>
 
-    <div class="name_of_page">
-        <div id="title">STOCKS INFORMATION</div>
-    </div>
+    <div id="title">STOCKS INFORMATION</div>
+
 
     <main>
         <a href='http://localhost/wst_page/rootfolder/main-admin.php'><button class='button1'>BACK TO MAIN</button></a>
@@ -124,101 +83,145 @@
 
     <br>
 
-    <?php
+    
+    <div class="glass2">
+
+    <div id="products2">
+
+        <div id="status">
+            <b>CURRENT STOCKS STATUS</b>
+            <br>
+            <br>
+
+        </div>
         
-        for($idx=0; $idx < count($stockid); $idx++){
-                            
-            if(empty($pname[$idx])){
 
-        
-               
-            }else{    ?>
-
-                        
-                        <main>
-
-                            <section class='glass'>
-
-                                <div id='products'>
-
-                                    <h3><?php echo "Product Name: &nbsp;" . $pname[$idx] ?></h3> 
-                                    
-                                        <table>
-
-                                            <tr>
-
-                                                <td>
-                                                    <a href='edit_stock.php? id=$stockid[$idx]'><button type= 'btn' class='button2'>EDIT</button></a> 
-                                                    <a href='http://localhost/wst_page/rootfolder/insidefolders/admin_product/admin.php'><button type= 'btn' class='button3'>PRODUCTS</button></a>
-                                                    &nbsp;
-                                                </td>
-                                                <td>
-                                                    <?php echo "Product Details:&nbsp;". $desc[$idx]; ?>
-                                                    <br>
-                                                    <br>
-                                                    
-                                                    <div id='stocks'>
-
-                                                    <?php 
-                                                    
-                                                    
-                                                        echo  "Available Stock: &nbsp;" ;
-                                                                       
-                                                    ?>
-                                                    
-                                                    <?php 
-         
-                                                        $total_stock = mysqli_query($conn, "SELECT * FROM `cart` as c LEFT JOIN `stocks` as s ON c.id = s.id;");
-                                                     
-                                                        if(mysqli_num_rows($total_stock) > 0){
-                                                            while($fetch_stocks = mysqli_fetch_assoc( $total_stock)){
-
-                                                            $stock_total = ($fetch_stocks['PRODUCT_QTY'] - $fetch_stocks['quantity']);
-                                                    ?>
-
-
-                                                        <?php  echo $stock_total; ?>
-
-                                                            
-                                                   
-
-
-                                                    <?php 
-                                                    
-                                                            }
-                                                    
-                                                        }
-                                                    ?>
-
-                                                    
-
-                                                    
-                                                    
-                                                    
-                                                        <br>
-                                                        <br>
-
-                                                    </div>
-                                                </td>
-                                 
-                                            </tr>
-
-                                        </table>
-
-                                </div>
    
-                            </section>
-   
-                        </main>
-   
-                        <br>
-                    
+
+
     <?php
-                }
 
-        }
+        $total_stock = mysqli_query($conn, "SELECT * FROM `cart` as c LEFT JOIN `stocks` as s ON c.id = s.id;");
+                                                                            
+        if(mysqli_num_rows($total_stock) > 0){
+            while($fetch_stocks = mysqli_fetch_assoc( $total_stock)){
 
     ?>
+
+    <table style="border:30px">
+
+        <tr>
+            <td>
+
+                <?php 
+
+               
+                    echo $fetch_stocks['name'] ;
+                
+                ?>
+
+            </td>
+
+            <td>
+
+                <?php    echo  $stock_total = ($fetch_stocks['PRODUCT_QTY'] - $fetch_stocks['quantity']) . "<br>" ;  ?>
+            
+            </td>
+
+
+
+        </tr>
+
+
+
+
+
+
+    </table>
+
+    <?php    }} ?>
+
+
+    </div>
+
+
+      
+    </div>
+
+    <?php
+
+
+        
+        for($idx=0; $idx < count($stockid); $idx++){
+
+            if(empty($pname[$idx])){
+
+
+
+
+            }else{
+
+                echo "
+
+                <main>
+
+                <section class='glass'>
+
+                    <div id='products'>
+
+                        <h3> Product Name: &nbsp;  $pname[$idx]</h3> 
+                        
+                            <table>
+
+                                <tr>
+
+                                    <td>
+                                        <a href='edit_stock.php? id=$stockid[$idx]'><button type= 'btn' class='button2'>EDIT</button></a> 
+                                        <a href='http://localhost/wst_page/rootfolder/insidefolders/admin_product/admin.php'><button type= 'btn' class='button3'>PRODUCTS</button></a>
+                                        &nbsp;
+                                    </td>
+                                    <td>
+                                        Product Details:&nbsp; $desc[$idx]
+                                        <br>
+                                        <br>
+
+                                        
+                                    <div id='stocks'>
+    
+                                       Starting Stock:   $prodQTY[$idx]  &nbsp;
+                                        <br>
+                                        <br>
+
+                                    </div>
+                                </td>
+                 
+                            </tr>
+
+                        </table>
+
+                </div>
+
+            </section>
+
+        </main>
+
+        <br>";
+            }
+
+           
+              
+             
+
+                                            }
+                                     
+
+                       
+                    
+ 
+   
+    ?>
+
+
 
   
 
